7c22ca2b27fb8d0e088bfa4f403d08064d5848da
Oliver McGibbon <472620@mymail.sussexdowns.ac.uk>
2017-12-08 11:19:24 +0000

    Initial commitment(Bannanaz... to the MOON!!!)

---
+++ Text adventure of Fae/Assets/TextManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour {
+
+    public Text text;
+    public Button trueButton;
+    public Button falseButton;
+
+    public bool option 1;
+    public bool option 2;
+
+    private enum States
+    {
+        question, trueState, falseState
+    };
+
+    private States mystate;
+
+}
+
+    // Use this for initialization
+    void Start () {
+    myStates = States.question;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onEnable() {
+    trueButton.onClick.AddListener(delegate { Option1 = true;});
+    falseButton.onClick.AddListener(delegate { Option2 = true; });
+
+}
+
+void question() { 
+        text.text = "Ice is cold.";
+    }
+
+    if(option1 == true){myState = States.trueState;}
+    else if(option2 == true){mystate = States.falseState;}
+    }
+}
